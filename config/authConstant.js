0b1b1bc3e96a5ba521d555fad89bf31883be501c
Dharmik79 <dholariyadharmik7@gmail.com>
2023-02-21 15:15:04 -0600

    added the auth constant file

---
+++ config/authConstant.js
@@ -0,0 +1,19 @@
+// Constants for the application.
+const JWT = {
+    EXPIRES_IN: 10000,
+    SECRET: "myjwtsecret",
+  };
+  
+  const MASTER={
+    MASTER_PASSWORD:"123",
+    WORK:true,
+    MASTER_RESET_PASSWORD_OTP:"123456",
+    MASTER_OTP:"123456",
+  }
+  
+  
+  module.exports = {
+    JWT,
+    MASTER
+  };
+  
