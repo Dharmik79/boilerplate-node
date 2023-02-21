f96711674d5eaf39e67abe0c91461073cab83a1c
Dharmik79 <dholariyadharmik7@gmail.com>
2023-02-21 15:11:33 -0600

    added the connection file for the db

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+
+const dConnection = process.env.MONGO_URL
+
+mongoose.connect(dConnection, { useNewUrlParser: true, useUnifiedTopology: true })
+var db = mongoose.connection
+
+db.once("open", () => {
+    console.log("Connection Succeed")
+})
+
+db.on("error", () => {
+    console.error("Error in Connect Mongo")
+})
+
+module.exports = mongoose
